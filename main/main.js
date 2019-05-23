e475c0a7f04e06a2b8292cf2116a4ae455ab1d73
Packsam <Packsam@yeah.net>
2019-05-23 14:43:50 +0800

    homework first version

--- main/main.js
+++ main/main.js
@@ -1,4 +1,9 @@
 // Write your cade below:
 module.exports = function main() {
-  
+    if(arguments[1]==0)
+    {
+        var calexception ="Divisor cannon be 0";
+        return calexception;
+    }
+    return(arguments[0]%arguments[1]);
 };
