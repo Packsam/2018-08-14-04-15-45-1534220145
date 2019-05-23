aa45c451c5fea2cc4059d3865003f01da17e0dc7
Packsam <Packsam@yeah.net>
2019-05-23 22:02:27 +0800

    exception handle

--- main/main.js
+++ main/main.js
@@ -1,7 +1,12 @@
 // Write your cade below:
 module.exports = function main() {
-    if(arguments[1]==0)
-    {
+    if (arguments.length!=2){
+        var calexception ="The input should be 2 elements";
+        return calexception;
+    }else if(isNaN(arguments[1])||isNaN(arguments[0])){
+        var calexception ="The input should be 2 numbers";   
+        return calexception; 
+    }else if(arguments[1]==0){
         var calexception ="Divisor cannon be 0";
         return calexception;
     }
