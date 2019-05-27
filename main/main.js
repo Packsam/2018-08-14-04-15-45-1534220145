49df68102c53fc86605e1cf1d7abeb575d36c214
Packsam <Packsam@yeah.net>
2019-05-27 17:44:32 +0800

    simplified version

--- main/main.js
+++ main/main.js
@@ -1,14 +1,11 @@
 // Write your cade below:
 module.exports = function main() {
     if (arguments.length!=2){
-        var calexception ="The input should be 2 elements";
-        return calexception;
+        return "The input should be 2 elements";
     }else if(isNaN(arguments[1])||isNaN(arguments[0])){
-        var calexception ="The input should be 2 numbers";   
-        return calexception; 
+        return "The input should be 2 numbers";   
     }else if(arguments[1]==0){
-        var calexception ="Divisor cannon be 0";
-        return calexception;
+        return "Divisor cannon be 0";
     }
     return(arguments[0]%arguments[1]);
 };
